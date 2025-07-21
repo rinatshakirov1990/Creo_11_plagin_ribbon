4a91931eaaf079fa51c226c52fd458757a1f1abb
JoraBoraTatarstan <Rinat.shakirov.2019@bk.ru>
2025-07-21 17:36:03 +0300

    плагин для creo_11 с кнопокой на ленте

---
+++ TestInstall.c
@@ -0,0 +1,15 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    int user_initialize()
+    {
+        return 0;
+    }
+
+    void user_terminate() {}
+    int user_main() { return 0; }
+
+#ifdef __cplusplus
+}
+#endif
